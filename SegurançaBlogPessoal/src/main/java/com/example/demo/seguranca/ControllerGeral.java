e27f083d353b4334c214320bc4f4934c619c5425
danielferreira3 <daniel.z.ferreira@hotmail.com>
2020-12-09 17:54:12 -0300

    criando segurança

---
+++ SegurançaBlogPessoal/src/main/java/com/example/demo/seguranca/ControllerGeral.java
@@ -0,0 +1,33 @@
+package com.example.demo.seguranca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Tema;
+import com.example.demo.repository.TemaRepository;
+
+@Controller
+public class ControllerGeral {
+
+	@Autowired
+	private TemaRepository repository;
+
+	// localhost:0808 <- direciona p a index
+	@GetMapping(value = "/")
+	public String login() {
+		return "index";
+	}
+
+	@GetMapping(value = "/cadastrarTema")
+	public String form() {
+		return "formTema";
+	}
+
+	@PostMapping(value="/cadastrarTema")
+	public String form(Tema tema){			
+		repository.save(tema);
+		return "redirect:/cadastrarTema";
+	}
+}
